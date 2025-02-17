404231ab5d2372da658655417f8d96ce7f551deb
abaars <abaars@sogyo.nl>
2025-02-17 15:31:29 +0100

    Added "GITLAB_ACCESS_TOKEN" on gitlab and added some important context to the backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,8 @@
+# This block refers to the location Terraform will store its state.
+# When using this method, terraform needs to be able to access the source (read & write)
+# Make an access token with API access and save it as a variable under "GITLAB_ACCESS_TOKEN"
+# Make sure it is masked and hidden, but not protected (otherwise it may not always trigger)
+# See https://stackoverflow.com/questions/69591195/why-is-tf-build-failing-with-error-refreshing-state-http-remote-state-endpoint
 terraform {
   backend "http" {
     address        = "https://git.sogyo.nl/api/v4/projects/4103/terraform/state/default"
