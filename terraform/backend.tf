3a63577c94cefd1e3f2e9e14b011159a7a8e370f
abaars <abaars@sogyo.nl>
2025-02-17 17:08:02 +0100

    End of day commit, need to continue working on the README

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,8 @@
 # When using this method, terraform needs to be able to access the source (read & write)
 # Make an access token with API access and save it as a variable under "GITLAB_ACCESS_TOKEN"
 # Make sure it is masked and hidden, but not protected (otherwise it may not always trigger)
-# See https://stackoverflow.com/questions/69591195/why-is-tf-build-failing-with-error-refreshing-state-http-remote-state-endpoint
+# Pass the GITLAB_ACCESS_TOKEN as an argument '-backend-config=password=${GITLAB_ACCESS_TOKEN}'
+# when running 'terraform init'.
 terraform {
   backend "http" {
     address         = "https://git.sogyo.nl/api/v4/projects/4103/terraform/state/default"
@@ -12,6 +13,5 @@ terraform {
     unlock_method   = "DELETE"
     retry_wait_min  = "5"
     username        = "abaars"
-    # password        = "${GITLAB_ACCESS_TOKEN}"
   }
 }
