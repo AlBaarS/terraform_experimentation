100e91eff93f966d4c11b216e52bea5fcd7485af
abaars <abaars@sogyo.nl>
2025-02-26 16:48:20 +0100

    Trying dynamic key creation

---
+++ create_token/main.tf
@@ -0,0 +1,26 @@
+# -[Resources/Datasources]-----------------------------------------------------
+
+# Generate an ssh keypair
+resource "tls_private_key" "rsa-4096-private" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Save the generated ssh keypair to Infisical
+resource "infisical_secret" "store-public-key-secret" {
+  count        = var.hosts
+  name         = "VM_KEY_PUBLIC"
+  value        = tls_private_key.rsa-4096-private.public_key_openssh
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/"
+}
+
+resource "infisical_secret" "store-private-key-secret" {
+  count        = var.hosts
+  name         = "VM_KEY_PRIVATE"
+  value        = tls_private_key.rsa-4096-private.private_key_openssh
+  env_slug     = "dev"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/"
+}
---
+++ create_token/variables.tf
@@ -0,0 +1,22 @@
+# -[Variables upon initialization]---------------------------------------------
+variable "hosts" {
+  default = 2
+}
+
+# -[Input variables]-----------------------------------------------------------
+variable "infisical_client_id" {
+  type = string
+  sensitive = true
+  ephemeral = true
+}
+
+variable "infisical_client_secret" {
+  type = string
+  sensitive = true
+  ephemeral = true
+}
+
+variable "infisical_workspace_id" {
+  type = string
+  sensitive = true
+}
