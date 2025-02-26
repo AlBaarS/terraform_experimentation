c3e878a05a28708fa0a895b82de73a4b8e0de791
abaars <abaars@sogyo.nl>
2025-02-26 15:01:20 +0100

    Attempt to reference the data source directly in the cloud_init.cfg

--- create_vms/variables.tf
+++ create_vms/variables.tf
@@ -32,11 +32,3 @@ variable "infisical_workspace_id" {
   type = string
   sensitive = true
 }
-
-variable "ssh_authorized_keys" {
-  type = list
-  default = [
-    data.infisical_secrets.common_secrets.secrets["SSHKEY_01"].value,
-    data.infisical_secrets.common_secrets.secrets["SSHKEY_02"].value
-  ]
-}
